d0f6ebdac380960fde5d87e0058271aa35090759
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-28 00:06:26 -0500

    Se agrega cors para evtar bloqueos por iconsistencias de ips

--- api/src/app.js
+++ api/src/app.js
@@ -6,12 +6,17 @@ import { create_roles } from './libs/initialSetup'
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
 
+import cors from 'cors'; // Import the cors package
+
+
 const app = express()
 //create_roles()
 
 app.set('pkg', pkg)
 app.use(morgan('dev'))
 app.set("json spaces", 4);
+
+app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => {
     res.json({
