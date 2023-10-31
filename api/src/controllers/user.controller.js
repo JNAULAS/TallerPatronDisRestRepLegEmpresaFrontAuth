35a184cf52e4c92d37cd9643fd621c1f494eb4f0
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-31 00:00:11 -0500

    Mapeo de datos de entrada para usuarios

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -3,16 +3,18 @@ import Role from '../models/role'
 
 export const createUser = async (req, res) => {
     try {
-        const {username, email, password, roles} = req.body
+        const {username, password, name, lastName, email, roles} = req.body
         const roles_found = await Role.find({ name: {$in: roles} })
         
-        const user = new User({ username, email, password, roles: roles_found.map( (role) => role._id ) })
+        const user = new User({ username, password, name, lastName, email, roles: roles_found.map( (role) => role._id ) })
         user.password = await User.encrypted_password(user.password)
         const saved_user = await user.save()
         
         return res.status(201).json({
             _id: saved_user._id,
             username: saved_user.username,
+            name: saved_user.name,
+            lastName: saved_user.lastName,
             email: saved_user.email,
             roles: saved_user.roles
         })
