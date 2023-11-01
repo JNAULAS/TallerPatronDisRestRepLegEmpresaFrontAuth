d9769993b722b30c487c3e30ef6a8b4d2c43c0a5
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-11-01 14:18:59 -0500

    Se agrega ruta para acceso a servicio de  verificación de tokens y permisos de administrador

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import * as authCtrl from '../controllers/auth.controller'
+import { is_admin, verify_token } from '../middlewares/auth.jwt'
 const router = Router()
 
 router.post('/signin', authCtrl.sign_in)
+// Verifica validez de tocken
+router.get('/verifica',verify_token, is_admin)
 
 export default router
