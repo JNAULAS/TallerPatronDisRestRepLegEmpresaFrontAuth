db5923cc29aaa89fb8efc828b27e1111a86d8104
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-11-01 14:17:41 -0500

    Se elimina paths innecesarios

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use( express.json() )
 app.use( express.urlencoded({extended:false}) )
 
 app.use('/users', userRoutes)
-app.use('/auth', authRoutes)
-app.use('/products', productsRoutes)
+//app.use('/auth', authRoutes)
+//app.use('/products', productsRoutes)
 
 export default app
